028639e21802333251415f99920b17c17e3bf790
Waitman Gobble <wago@waitman.net>
2021-09-02 22:58:37 -0400

    add comments to mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -20,18 +20,28 @@ public:
     ~MainWindow();
 
 private slots:
+    /* go ahead and load the URL when the button is clicked */
     void on_goButton_clicked();
+    /* load the URL when the user clicks on a row in the history table */
     void on_historyTable_clicked(const QModelIndex &index);
+    /* wipe the history table */
     void on_clearHistoryButton_clicked();
+    /* update the config database table when the proxy host value is edited */
     void on_proxyUrlEdit_textChanged(const QString &arg1);
+    /* update the config database table when the proxy port value is edited */
     void on_proxyPortEdit_textChanged(const QString &arg1);
 
 private:
     Ui::MainWindow *ui;
+    /* a model connected to the database linked to the table widget */
     QSqlTableModel *model;
+    /* the db connection */
     QSqlDatabase db;
+    /* config key/value hash that is not yet necessary */
     QHash<QString, QString> config;
+    /* proxy info */
     QNetworkProxy proxy;
+    /* we set loading = true during startup so the update triggers are not executed */
     bool loading;
 };
 #endif // MAINWINDOW_H
